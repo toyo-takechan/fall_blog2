143c4cf4ad19a74095b12859d629fbfded415404
toyo-takechan <toyo-takechan@mtf.biglobe.ne.jp>
2022-08-21 12:58:04 +0900

    fulltitle firsttest

--- test/controllers/homes_controller_test.rb
+++ test/controllers/homes_controller_test.rb
@@ -4,5 +4,24 @@ class HomesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get root_url
     assert_response :success
+    assert_select "title", "FallBlog"
+  end
+
+  test "should get about" do
+    get about_path
+    assert_response :success
+    assert_select "title", "FallBlog  |  about"
+  end
+
+  test "should get play" do
+    get play_path
+    assert_response :success
+    assert_select "title", "FallBlog  |  play"
+  end
+
+  test "should get practice" do
+    get practice_path
+    assert_response :success
+    assert_select "title", "FallBlog  |  practice"
   end
 end
