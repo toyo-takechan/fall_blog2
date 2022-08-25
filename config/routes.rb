0569b92e420116005d017958b2a8cd8328d6978c
toyo-takechan <toyo-takechan@mtf.biglobe.ne.jp>
2022-08-26 08:36:52 +0900

    user new user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  # get 'homes/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "homes#index"
-
   get '/about' ,   to: 'homes#about'
   get '/practice', to: 'homes#practice'
   get '/play',     to: 'homes#play'
+  resources :users
 end
