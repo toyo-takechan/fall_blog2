8f116662620790dd449bb1021d37a68e6dce01ba
toyo-takechan <toyo-takechan@mtf.biglobe.ne.jp>
2022-08-26 15:43:19 +0900

    user moder test first

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get '/about' ,   to: 'homes#about'
   get '/practice', to: 'homes#practice'
   get '/play',     to: 'homes#play'
+  get '/signup',   to: 'users#new'
   resources :users
 end
