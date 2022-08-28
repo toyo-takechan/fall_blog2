a8d43a7dfff7580f7f169f8b6a2a93cb0d257ff6
toyo-takechan <toyo-takechan@mtf.biglobe.ne.jp>
2022-08-28 10:39:01 +0900

    log_in user log_out

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,18 @@ module ApplicationHelper
     session[:user_id] = user.id
   end
 
+  def current_user
+    if session[:user_id]
+    @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def log_out
+    session.delete(:user_id)
+    current_user = nil
+  end
 end
