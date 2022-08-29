e903ed6e932b30ee2f5570aa638b0a5035d59b70
toyo-takechan <toyo-takechan@mtf.biglobe.ne.jp>
2022-08-29 18:36:04 +0900

    frendly forwrding

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -33,4 +33,15 @@ module ApplicationHelper
     session.delete(:user_id)
     current_user = nil
   end
+
+  # 記憶したURL（もしくはデフォルト値）にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
