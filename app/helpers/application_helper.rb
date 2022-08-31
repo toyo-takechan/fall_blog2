408113b42909d3fd359cb610c0bcc3af52861cd5
toyo-takechan <toyo-takechan@mtf.biglobe.ne.jp>
2022-08-31 11:24:07 +0900

    cookies meshod

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,12 +14,31 @@ module ApplicationHelper
     session[:user_id] = user.id
   end
 
-  def current_user
-    if session[:user_id]
-    @current_user ||= User.find_by(id: session[:user_id])
-    end
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
 
+  #セッションだけのカレントユーザー
+  # def current_user
+  #   if session[:user_id]
+  #   @current_user ||= User.find_by(id: session[:user_id])
+  #   end
+  # end
+
+# 記憶トークンcookieに対応するユーザーを返す
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.encrypted[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
 # 渡されたユーザーがカレントユーザーであればtrueを返す
   def current_user?(user)
     user && user == current_user
@@ -29,7 +48,16 @@ module ApplicationHelper
     !current_user.nil?
   end
 
+  #永続セッションを破壊する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+# 現在のユーザーをログアウトする
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     current_user = nil
   end
