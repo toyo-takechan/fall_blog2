7c96639d7b7a62d685419d23f53669e963af0b4d
toyo-takechan <toyo-takechan@mtf.biglobe.ne.jp>
2022-09-04 12:30:20 +0900

    password-reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,8 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user,         only: [:edit, :update]
+  before_action :valid_user,       only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
+
   def new
   end
 
@@ -16,5 +20,45 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
+  end
+
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, :blank)
+      rendet 'edit'
+    elsif @user.update(user_params)
+      log_in @user
+      flash[:success] = "パスワードの再設定が完了しました。"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
+  # beforeフィルタ
+
+  def get_user
+    @user = User.find_by(email: params[:email])
+  end
+
+  # 正しいユーザーかどうか確認する
+  def valid_user
+    unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
+      redirect_to root_url
+    end
+  end
+
+  # トークンが期限切れかどうか確認する
+  def check_expiration
+    if @user.password_reset_expired?
+      flash[:danger] = "パスワード再設定の有効期限切れです。"
+      redirect_to new_password_reset_url
+    end
   end
 end
